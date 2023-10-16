f8ef0448cd0ced3fbd0192e2513ebfd83bff9777
CloudDev-Harsh <dev.harshpal@gmail.com>
2023-10-16 18:10:05 +0530

    updated the table

--- spring-data-rev/src/main/java/com/mycodesuffer/spring/data/jpa/entity/Student.java
+++ spring-data-rev/src/main/java/com/mycodesuffer/spring/data/jpa/entity/Student.java
@@ -1,7 +1,6 @@
 package com.mycodesuffer.spring.data.jpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,15 +11,50 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(
+        name = "tbl_student",
+        uniqueConstraints = @UniqueConstraint(
+                name = "email_id_unique",
+                columnNames = "email_address"
+        )
+)
 public class Student {
 
     @Id
+    @Column(name = "student_id")
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long studentId;
+
+    @Column(
+            name = "first_name",
+            nullable = false
+    )
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(
+            name = "email_address",
+            nullable = false
+    )
     private String emailId;
+
+    @Column(name = "guardian_name")
     private String guardianName;
+
+    @Column(name = "guardian_email")
     private String guardianEmailId;
+
+    @Column(name = "guardian_phone_no")
     private String guardianPhoneNo;
 
 
